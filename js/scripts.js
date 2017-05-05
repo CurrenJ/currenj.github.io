67ce8af29fc00fb35ce253c921d0ca23a80bd53d
CurrenJ <currenjeandell@me.com>
2017-05-05 12:38:05 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $("#com").hide();
     
     $.when( $("#cj").fadeIn(1500) ).done(function() {
+        $('.whole').animate({
+          right: '200px'
+        });
         $("#com").fadeIn(4000);
     });
 });
