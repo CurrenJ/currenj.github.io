e9fd2a8f7f56114b58291c2307cee0c9974d9f0f
CurrenJ <jeandell.curren@gmail.com>
2019-09-27 06:59:38 -0700

    Updated landing page
    Looks decent now

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {   
-    $("#cj").hide();
-    $("#com").css('opacity','0')
-    $.when( $("#cj").fadeIn(1500) ).done(function() {
-        $("#com").fadeTo(4000, 1);
-        $("#com").animate("left", "-10vw");
-    });
-});
-
