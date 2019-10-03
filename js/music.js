407bbcea10e1f4ae11569ba6ee5efb006e658bfc
CurrenJ <jeandell.curren@gmail.com>
2019-10-03 09:51:37 +0100

    Better wave gen

--- js/music.js
+++ js/music.js
@@ -5,14 +5,14 @@ $( function() {
 });
 
 function drawWave(svg, amplitude) {
-	var contHeight = $(svg).css("height");
+	var contHeight = parseInt($(svg).css("height"));
 	var c2 = contHeight / 2;
 	var origin = { //origin of axes
 		x: 0,
 		y: 75
 	};
 	//var amplitude = 10; // wave amplitude
-	var rarity = 1; // point spacing
+	var rarity = 5; // point spacing
 	var freq = Math.PI * 2 / parseInt($("body").css("width")) * rarity; // angular frequency
 	console.log($("body").css("width"));
 	var phase = 0; // phase angle
@@ -20,53 +20,64 @@ function drawWave(svg, amplitude) {
 	console.log(contHeight);
 
 	for (var i = 0; i < parseInt($("body").css("width")) * 2; i++) {
-		var line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-
-		line1.setAttribute('x1', (i - 1) * rarity + origin.x);
-		line1.setAttribute('y1', Math.sin(freq*(i - 1 + phase)) * amplitude + origin.y);
-
-		line1.setAttribute('x2', (i - 1) * rarity + origin.x);
-		line1.setAttribute('y2', contHeight);
-
-		line1.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
-
-		svg.appendChild(line1);
+		var polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+		var points = "";
+		points += ((i - 1) * rarity + origin.x) + " " + (Math.sin(freq*(i - 1 + phase)) * amplitude + origin.y) + ", ";
+		points += ((i - 1) * rarity + origin.x) + " " + contHeight + ", ";
+		points += (i * rarity + origin.x) + " " + contHeight + ", ";
+		points += (i * rarity + origin.x) + " " + (Math.sin(freq*(i + phase)) * amplitude + origin.y) + ", ";
+		polygon.setAttribute("points", points);
+		polygon.setAttribute("style", "stroke:white;stroke-width:1;fill:white");
 		
-		var line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-
-		line2.setAttribute('x1', (i - 1) * rarity + origin.x);
-		line2.setAttribute('y1', contHeight);
-
-		line2.setAttribute('x2', i * rarity + origin.x);
-		line2.setAttribute('y2', contHeight);
-
-		line2.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
-
-		svg.appendChild(line2);
+		svg.appendChild(polygon);
 		
-		var line3 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+		// var line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
-		line3.setAttribute('x1', i * rarity + origin.x);
-		line3.setAttribute('y1', contHeight);
+		// line1.setAttribute('x1', (i - 1) * rarity + origin.x);
+		// line1.setAttribute('y1', Math.sin(freq*(i - 1 + phase)) * amplitude + origin.y);
 
-		line3.setAttribute('x2', i * rarity + origin.x);
-		line3.setAttribute('y2', Math.sin(freq*(i + phase)) * amplitude + origin.y);
+		// line1.setAttribute('x2', (i - 1) * rarity + origin.x);
+		// line1.setAttribute('y2', contHeight);
 
-		line3.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
+		// line1.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
 
-		svg.appendChild(line3);
+		// svg.appendChild(line1);
 		
-		var line4 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+		// var line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
-		line4.setAttribute('x2', (i - 1) * rarity + origin.x);
-		line4.setAttribute('y2', Math.sin(freq*(i - 1 + phase)) * amplitude + origin.y);
+		// line2.setAttribute('x1', (i - 1) * rarity + origin.x);
+		// line2.setAttribute('y1', contHeight);
 
-		line4.setAttribute('x1', i * rarity + origin.x);
-		line4.setAttribute('y1', Math.sin(freq*(i + phase)) * amplitude + origin.y);
+		// line2.setAttribute('x2', i * rarity + origin.x);
+		// line2.setAttribute('y2', contHeight);
 
-		line4.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
+		// line2.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
 
-		svg.appendChild(line4);
+		// svg.appendChild(line2);
+		
+		// var line3 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+		// line3.setAttribute('x1', i * rarity + origin.x);
+		// line3.setAttribute('y1', contHeight);
+
+		// line3.setAttribute('x2', i * rarity + origin.x);
+		// line3.setAttribute('y2', Math.sin(freq*(i + phase)) * amplitude + origin.y);
+
+		// line3.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
+
+		// svg.appendChild(line3);
+		
+		// var line4 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+		// line4.setAttribute('x2', (i - 1) * rarity + origin.x);
+		// line4.setAttribute('y2', Math.sin(freq*(i - 1 + phase)) * amplitude + origin.y);
+
+		// line4.setAttribute('x1', i * rarity + origin.x);
+		// line4.setAttribute('y1', Math.sin(freq*(i + phase)) * amplitude + origin.y);
+
+		// line4.setAttribute('style', "stroke:white;stroke-width:1;fill:white;fill-rule=\"nonzero\"");
+
+		// svg.appendChild(line4);
 		
 		
 	}
