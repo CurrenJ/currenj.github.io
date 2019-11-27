f1595a69bc7c53c098287cc8a3e8b0bc47496726
CurrenJ <jeandell.curren@gmail.com>
2019-11-27 12:24:32 +0000

    have a little salami, my dear bernie!
    bernie wants some salami

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function() {
 	console.log("Ready!");
 	$("#title-wrapper").after("<span id=\"randomText\"></span>");
     var rText = $("#randomText");
-	var textList = ["is Dennis Reynolds.", "believes in Froggyland.", "won't let you down.", "believes in you!"];
+	var textList = ["demonstrates his value.", "believes in Froggyland.", "won't let you down.", "believes in you!", "supports milkboat!", "A professional.", "codes, codes, codes."];
 	console.log(textList[Math.floor(Math.random() * textList.length)]);
 	rText.html(textList[Math.floor(Math.random() * textList.length)]);
 });
