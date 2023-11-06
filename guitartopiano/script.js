07057966899f244d80e97471653a5780aadeee57
Curren <jeandell.curren@gmail.com>
2023-11-06 22:16:56 +0000

    Add My Music Journal note-taking section

--- guitartopiano/script.js
+++ guitartopiano/script.js
@@ -86,7 +86,194 @@ function updateDisplay()
   convertText(document.getElementById("guitarChordText").value)
 }
 
+
 window.onload = function() {
 	updateDisplay();
 };
 
+
+
+// SONG STUFF
+
+function saveSong(songDOM)
+{
+	songObj = getNewSongObj();
+	console.log(songDOM);
+	songObj["title"] = songDOM.getElementsByClassName("song-title")[0].innerHTML;
+	chords = songDOM.getElementsByClassName("song-chords")[0].getElementsByClassName("song-chord");
+	songObj["chords"] = [];
+	for (let i = 0; i < chords.length; i++)
+	{
+		chord = chords[i].getElementsByTagName("SPAN")[0].innerHTML;
+		songObj["chords"].push(chord);
+	}
+	songObj["info"] = songDOM.getElementsByClassName("song-description")[0].innerHTML;
+
+
+	songJSON = JSON.stringify(songObj)
+	var bb = new Blob([songJSON], { type: 'text/plain' });
+	var a = document.createElement('a');
+	a.download = songObj["title"].replace(/[^a-zA-Z0-9]/g, '')+ '.txt';
+	a.href = window.URL.createObjectURL(bb);
+	a.click();
+}
+
+function loadSong()
+{
+	var input = document.createElement('input');
+	input.type = 'file';
+	input.click();
+
+	input.onchange = e => { 
+
+		// getting a hold of the file reference
+		var file = e.target.files[0]; 
+	 
+		// setting up the reader
+		var reader = new FileReader();
+		reader.readAsText(file,'UTF-8');
+	 
+		// here we tell the reader what to do when it's done reading...
+		reader.onload = readerEvent => {
+		   var content = readerEvent.target.result; // this is the content!
+		   addSongToDOM(JSON.parse(content));
+		}
+	 
+	}	 
+}
+
+function addSongToDOM(songObj)
+{
+	song = document.createElement("DIV");
+	song.setAttribute("CLASS", "song");
+
+	// Song Title
+	songTitle = document.createElement("SPAN");
+	songTitle.setAttribute("CLASS", "song-title");
+	songTitle.setAttribute("CONTENTEDITABLE", "true")
+	songTitle.innerHTML = songObj["title"];
+	song.appendChild(songTitle);
+
+	songDescription = document.createElement("P");
+	songDescription.setAttribute("CLASS", "song-description");
+	songDescription.setAttribute("CONTENTEDITABLE", "true");
+	songDescription.innerHTML = songObj["info"]
+	song.appendChild(songDescription)
+
+	// Song Chords
+	songChords = document.createElement("UL");
+	songChords.setAttribute("CLASS", "song-chords no-bullets");
+
+	songObj["chords"].forEach(chord => {
+		addChordToDOM(chord, songChords);
+	});
+	song.appendChild(songChords);
+
+
+	// Song Save / Add Chord Buttons
+	songAddChordButton = document.createElement("INPUT");
+	songAddChordButton.setAttribute("TYPE", "button");
+	songAddChordButton.setAttribute("CLASS", "song-add-chord");
+	songAddChordButton.setAttribute("VALUE", "Add Chord");
+	songAddChordButton.onclick = function(e){ 
+		addChordToDOM(document.getElementById("guitarChordText").value, e.target.parentNode.getElementsByClassName("song-chords")[0]);
+	};
+	song.appendChild(songAddChordButton);
+
+	songSaveJSONButton = document.createElement("INPUT");
+	songSaveJSONButton.setAttribute("TYPE", "button");
+	songSaveJSONButton.setAttribute("CLASS", "song-save song-button");
+	songSaveJSONButton.setAttribute("VALUE", "Save as JSON");
+	songSaveJSONButton.onclick = function(e){
+		saveSong(e.target.parentNode);
+	};
+	song.appendChild(songSaveJSONButton);
+
+	document.getElementById("songs").appendChild(song);
+}
+
+function addChordToDOM(chordString, parentEl){
+	songChord = document.createElement("LI");
+	songChord.setAttribute("CLASS", "song-chord");
+	songChord.setAttribute("DRAGGABLE", "true");
+	songChord.setAttribute("ONDRAGOVER", "dragOver(event)");
+	songChord.setAttribute("ONDRAGSTART", "dragStart(event)");
+
+	songChordText = document.createElement("SPAN");
+	songChordText.setAttribute("CLASS", "song-chord-text");
+	songChordText.innerHTML=chordString;
+	songChordText.onclick = function(e){ document.getElementById("guitarChordText").value = e.target.innerHTML; updateDisplay(); } ;
+	songChord.appendChild(songChordText);
+
+	songChordSet = document.createElement("INPUT");
+	songChordSet.setAttribute("TYPE", "button");
+	songChordSet.setAttribute("VALUE", "Set");
+	songChordSet.onclick = function(e){ 
+		e.target.parentNode.getElementsByClassName("song-chord-text")[0].innerHTML = document.getElementById("guitarChordText").value;
+	};
+	songChordSet.setAttribute("CLASS", "song-chord-set song-button")
+	songChord.appendChild(songChordSet);
+	
+	songChordDelete = document.createElement("INPUT");
+	songChordDelete.setAttribute("TYPE", "button");
+	songChordDelete.setAttribute("VALUE", "Remove");
+	songChordDelete.onclick = function(e){ 
+		e.target.parentNode.remove();
+	};
+	songChordDelete.setAttribute("CLASS", "song-chord-delete song-button")
+	songChord.appendChild(songChordDelete);
+
+	parentEl.appendChild(songChord);
+}
+
+function newSong()
+{
+	addSongToDOM(getNewSongObj());
+}
+
+function getNewSongObj()
+{
+	songObj = 
+	{
+		"title": "New Song",
+		"chords": ["1,2,3,4,5,6", "0,0,0,0,0,0"],
+		"tuning": "EADGBE",
+		"info": "Some information about your song."
+	};
+	return songObj;
+}
+
+
+// Drag & Drop List from @davidf
+
+var _el;
+
+function dragOver(e) {
+	var target = e.target;
+	while (target.parentNode.tagName != 'UL') {
+		target = target.parentNode;
+	}
+
+  if (isBefore(_el, target))
+    target.parentNode.insertBefore(_el, target);
+  else
+    target.parentNode.insertBefore(_el, target.nextSibling);
+}
+
+function dragStart(e) {
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/plain", null); // Thanks to bqlou for their comment.
+  var target = e.target;
+  while (target.parentNode.tagName != 'UL') {
+	  target = target.parentNode;
+  }
+  _el = target;
+}
+
+function isBefore(el1, el2) {
+  if (el2.parentNode === el1.parentNode)
+    for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling)
+      if (cur === el2)
+        return true;
+  return false;
+}
